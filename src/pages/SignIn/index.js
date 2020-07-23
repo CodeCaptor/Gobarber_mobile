8ee5d070d071d39f71270fd9418880015a0d7c54
KZTN <kakaiocesar2@gmail.com>
2020-07-23 19:59:56 -0300

    :sparkles: Created linear gradient Background component
    all changes are functional

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Text } from 'react-native';
+import { Background } from '../../components/Background';
 
-// import { Container } from './styles';
-
-const SignIn = () => {
-    return <Text> SignIn</Text>;
-};
-
-export default SignIn;
+export default function SignIn() {
+    return (
+        <Background>
+            <Text>SignIn</Text>
+        </Background>
+    );
+}
