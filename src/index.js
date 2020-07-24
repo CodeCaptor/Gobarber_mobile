5ac064fd3aec6b5a8f7b9cafe819dff9f7d337e5
KZTN <kakaiocesar2@gmail.com>
2020-07-24 17:27:55 -0300

    :children_crossing: Created globalstyles using SafeAreaView
    Prevent elements being displayed to unappropriate screens regions

--- src/index.js
+++ src/index.js
@@ -6,14 +6,16 @@ import './config/ReactotronConfig';
 import { StatusBar } from 'react-native';
 import { persistor, store } from './store';
 import App from './App';
-// import { Container } from './styles';
+import { Container } from './style/global';
 
 export default function Index() {
     return (
         <Provider store={store}>
             <PersistGate persistor={persistor}>
                 <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-                <App />
+                <Container>
+                    <App />
+                </Container>
             </PersistGate>
         </Provider>
     );
