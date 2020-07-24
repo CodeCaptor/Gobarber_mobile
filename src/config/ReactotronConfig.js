61c278d81dee8ec534eb28ee64fd3feaf645c6af
KZTN <kakaiocesar2@gmail.com>
2020-07-24 14:06:26 -0300

    :bug: Fixing ReactotronConfigure structure
    all changes is fixed

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,12 +1,12 @@
 import Reactotron from 'reactotron-react-native';
-import ReactotronRedux from 'reactotron-redux';
-import ReactotronSaga from 'reactotron-redux-saga';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 if (__DEV__) {
     const tron = Reactotron.configure({ host: '192.168.18.250' })
         .useReactNative()
-        .use(ReactotronRedux())
-        .use(ReactotronSaga)
+        .use(reactotronRedux())
+        .use(reactotronSaga())
         .connect();
 
     tron.clear();
