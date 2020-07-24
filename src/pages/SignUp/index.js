5e1338cb1499cac3b0b889b9aabdde0c4aae0097
KZTN <kakaiocesar2@gmail.com>
2020-07-23 21:25:07 -0300

    :sparkles: Created SignUp page
    This is a static visual version of the SignIn page. More content and improvements needs to be implemented

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,46 @@
 import React from 'react';
-import { Text } from 'react-native';
+import Logo from '../../assets/logo.png';
+import {
+    Container,
+    Image,
+    Form,
+    FormInput,
+    SubmitButton,
+    SignLinkText,
+    SignLink,
+} from './styles';
 import { Background } from '../../components/Background';
 
-export default function SignUp() {
+export default function SignUp({ navigation }) {
     return (
         <Background>
-            <Text>SignUp</Text>
+            <Container>
+                <Image source={Logo} />
+                <Form>
+                    <FormInput
+                        icon="person-outline"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Digite seu nome completo"
+                    />
+                    <FormInput
+                        icon="mail-outline"
+                        keyboardType="email-address"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Digite seu email"
+                    />
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Digite sua senha"
+                    />
+                    <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+                </Form>
+                <SignLink onPress={() => navigation.navigate('SignIn')}>
+                    <SignLinkText>Já possuo cadastro</SignLinkText>
+                </SignLink>
+            </Container>
         </Background>
     );
 }
