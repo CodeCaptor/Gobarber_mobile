c79c938425a8f69342e8f9251185f68c900be90c
KZTN <kakaiocesar2@gmail.com>
2020-07-25 18:03:23 -0300

    :art: Integrated API to the Dashboard page
    Representations, buttons effects and content is now visible and functional

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Left, Avatar, Info, Time, Name } from './styles';
+import api from '../../services/api';
 
-export default function Appointment() {
+export default function Appointment({ data, onCancel }) {
+    const dateParsed = useMemo(() => {
+        return formatRelative(parseISO(data.date), new Date(), {
+            locale: pt,
+            addSufix: true,
+        });
+    }, [data.date]);
+
     return (
-        <Container>
+        <Container past={data.past}>
             <Left>
                 <Avatar
                     source={{
-                        uri:
-                            'https://api.adorable.io/avatars/50/rocketseat.png',
+                        uri: data.provider.avatar
+                            ? data.provider.avatar.url
+                            : 'https://api.adorable.io/avatars/50/rocketseat.png',
                     }}
                 />
                 <Info>
-                    <Name>Diego Fernandes</Name>
-                    <Time>Em 3 horas</Time>
+                    <Name>{data.provider.name}</Name>
+                    <Time>{dateParsed}</Time>
                 </Info>
             </Left>
-            <TouchableOpacity onPress={() => {}}>
-                <Icon name="event-busy" size={20} color="#f64c75" />
-            </TouchableOpacity>
+            {data.cancelable && (
+                <TouchableOpacity onPress={onCancel}>
+                    <Icon name="event-busy" size={20} color="#f64c75" />
+                </TouchableOpacity>
+            )}
         </Container>
     );
 }
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 import { Container, Title, List } from './styles';
 import { Background } from '../../components/Background';
 import Appointment from '../../components/Appointment';
 
 const data = [1, 2, 3, 4, 5];
 export default function Dashboard() {
+    const [appointments, setAppointments] = useState([]);
+
+    useEffect(() => {
+        async function getAppointmentsData() {
+            const response = await api.get('appointments');
+            setAppointments(response.data);
+        }
+        getAppointmentsData();
+    });
+    async function handleCancel(id) {
+        const response = await api.delete(`appointments/${id}`);
+        setAppointments(
+            appointments.map((appointment) =>
+                appointment.id === id
+                    ? { ...appointment, canceled_at: response.data.canceled_at }
+                    : appointment
+            )
+        );
+    }
     return (
         <Background>
             <Container>
                 <Title>Agendamentos</Title>
                 <List
-                    data={data}
-                    KeyExtractor={(item) => String(item)}
-                    renderItem={({ item }) => <Appointment data={item} />}
+                    data={appointments}
+                    KeyExtractor={(item) => String(item.id)}
+                    renderItem={({ item }) => (
+                        <Appointment
+                            onCancel={() => handleCancel(item.id)}
+                            data={item}
+                        />
+                    )}
                 />
             </Container>
         </Background>
