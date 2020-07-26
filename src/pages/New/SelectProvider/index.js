f0639bd0534a2ecfdcdddfba6c1280143a1e7c95
KZTN <kakaiocesar2@gmail.com>
2020-07-26 12:02:20 -0300

    :construction: Working on New page
    Created initial features in SelectDateTime stack page

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../../services/api';
 import { Background } from '../../../components/Background';
-// import { Container } from './styles';
 
-export default function SelectProvider() {
-    return <Background />;
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
+
+export default function SelectProvider({ navigation }) {
+    const [providers, setProviders] = useState([]);
+    useEffect(() => {
+        async function loadProvidersData() {
+            const response = await api.get('providers');
+            setProviders(response.data);
+        }
+        loadProvidersData();
+    }, []);
+    return (
+        <Background>
+            <Container>
+                <ProvidersList
+                    data={providers}
+                    keyExtractor={(provider) => String(provider.id)}
+                    renderItem={(item) => (
+                        <Provider
+                            onPress={() =>
+                                navigation.navigate('SelectDateTime', { item })
+                            }
+                        >
+                            <Avatar
+                                source={{
+                                    uri: item.item.avatar
+                                        ? item.item.avatar.url
+                                        : 'https://api.adorable.io/avatars/50/rocketseat.png',
+                                }}
+                            />
+                            <Name>{item.item.name}</Name>
+                        </Provider>
+                    )}
+                />
+            </Container>
+        </Background>
+    );
 }
 
 SelectProvider.navigationOptions = ({ navigation }) => ({
